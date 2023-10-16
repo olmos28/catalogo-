98fd1b4e00d3309dd5a08fa7e5c961353334376d
olmos28 <146895629+olmos28@users.noreply.github.com>
2023-10-16 20:34:30 +0200

    Create catalogo.js

---
+++ catalogo.js
@@ -0,0 +1,59 @@
+console.log("MIA COLUCCI");
+
+console.log("1.sueter $200 codigo: 111");
+var p1=10;
+console.log("2.playera $150 codigo: 222");
+var p2=10;
+console.log("3.chamara de piel $900 codigo: 333");
+var p3=10;
+console.log("4.zapatillas $400 codigo: 444");
+var p4=10;
+console.log("5.tops con trasparencia $250 codigo: 555");
+var p5=10;
+console.log("6.corset princesa $500 codigo: 666");
+var p6=10;
+console.log("7.gorra ken $250 codigo: 777");
+var p7=10;
+console.log("8.vestidos barbie $2000 codigo: 888");
+var p8=10;
+console.log("9.botas versache $5000 codigo: 999");
+var p9=10;
+console.log("10.bolsa gucci $10000 codigo: 101010");
+var p10=10;
+
+var producto = parseInt(prompt("ingresa el codigo del producto que desees adquirir"));
+switch(producto){
+    
+    case 555:
+    console.log("sueter $200 y existen "+p1+" productos");
+    console.log
+    break;
+    case 111:
+    console.log("playera $150 y existen "+p2+" productos");
+    break;
+    case 222:
+    console.log("chamara de piel $900 y existen "+p3+" productos");
+    break;
+    case 333:
+    console.log("zapatillas $400 y existen "+p4+" productos");
+    break;
+    case 444:
+    console.log("tops con trasparencia $250 y existen "+p5+" productos");
+    break;
+    case 555:
+    console.log("6.corset princesa $500 y existen "+p6+" productos");
+    break;
+    case 666:
+    console.log("7.gorra ken $250 y existen "+p7+" productos");
+    break;
+    case 777:
+    console.log("8.vestidos barbie $2000 "+p8+" productos");
+    break;
+    case 888:
+    console.log("9.botas versache $5000 "+p9+" productos");
+    break;
+    case 999:
+    console.log("10.bolsa gucci $10000 "+p10+" productos");
+    break;
+    case 101010:
+}
